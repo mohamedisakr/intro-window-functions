5b50946c3bb8ae00788d9542c72b0505c8484c0d
mohamedisakr <mohamedisakr@gmail.com>
2022-09-14 11:49:43 +0200

    rank & dense_rank function

--- 002_rank_vs_dense_rank/window.sql
+++ 002_rank_vs_dense_rank/window.sql
@@ -1,8 +1,16 @@
 -- Like row_number, but ties have the value of the first row in the group
--- Wit rank, after a tie, row numbers begin counting at the row number again (1, 2, 2, 4)
+-- With rank, after a tie, row numbers begin counting at the row number again (1, 2, 2, 4)
 -- With dense_rank, row numbers begin counting at the next number (1, 2, 2, 3)
 -- See division 1, rank 20
 
+SELECT division, DENSE_RANK() OVER (PARTITION BY division ORDER BY pace ASC) AS ranking,  pace, name
+FROM runners
+LIMIT 100
+
+
+
+
+/*
 SELECT
 division,
 rank() OVER (PARTITION BY division ORDER BY pace ASC),
@@ -16,3 +24,4 @@ dense_rank() OVER (PARTITION BY division ORDER BY pace ASC),
 pace,
 name
 FROM runners;
+*/
